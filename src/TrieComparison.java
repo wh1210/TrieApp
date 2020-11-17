043cc560443fd813a7ca894025e571fd3e9cba22
wh1210 <hhhhhwu@google.com>
2020-11-16 22:30:37 -0700

    add words text

---
+++ src/TrieComparison.java
@@ -0,0 +1,11 @@
+package src;
+
+public class TrieComparison {
+    public static void main(String[] args) {
+        System.out.println("x");
+    }
+
+    public static String[] readWords() {
+        return null;
+    }
+}
