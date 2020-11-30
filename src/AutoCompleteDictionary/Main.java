055f033cd636e3023af4ded2ba6431ff94f29fb9
wh1210 <hhhhhwu@google.com>
2020-11-29 18:13:07 -0700

    add interaction

--- src/AutoCompleteDictionary/Main.java
+++ src/AutoCompleteDictionary/Main.java
@@ -5,13 +5,53 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         DictTrie dictionary = new DictTrie();
         initializeDict(dictionary);
-        dictionary.search("dicta");
-        System.out.println(dictionary.wordRecommendation("dict"));
+        System.out.println("Please choose the operation(enter the number): ");
+        System.out.println("1: search if the word exists");
+        System.out.println("2: word recommendation");
+        System.out.println("3: exit");
+        Scanner console = new Scanner(System.in);
+        operations(dictionary, console);
+    }
+
+    public static void operations(DictTrie dictionary, Scanner console) {
+        String operation;
+        boolean keeping = true;
+        while (keeping) {
+            operation = console.nextLine();
+            if (operation.equals("1")) {
+                System.out.println("Please input the word you want to check:");
+                String word = console.nextLine();
+                boolean exist = dictionary.search(word);
+                if (exist) {
+                    System.out.println("This word exists! You can continue by input operation number.");
+                } else {
+                    System.out.println("This word doesn't exist! You can continue by input operation number.");
+                }
+            } else if (operation.equals("2")) {
+                System.out.println("Please input the prefix you want to search for:");
+                String prefix = console.nextLine();
+                System.out.println("These are top 10(if there are) words starting with this prefix(tie-breaker by your search frequency)");
+                System.out.println(dictionary.wordRecommendation(prefix));
+                System.out.println("You can continue by input operation number.");
+            } else if (operation.equals("3")) {
+                System.out.println("Thanks for using this dictionary!");
+                keeping = false;
+                return;
+            } else {
+                System.out.println("Input is invalid, please input again.");
+                System.out.println("Please choose the operation(enter the number): ");
+                System.out.println("1: search if the word exists");
+                System.out.println("2: word recommendation");
+                System.out.println("3: exit");
+            }
+        }
+        
     }
 
     public static List<String> readWords(String file) {
